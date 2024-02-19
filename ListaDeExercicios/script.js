8e08949ad653e3d0e003f0ba2cfeee5cec0f9372
Giselle <o.giselle@escola.pr.gov.br>
2024-02-19 10:34:18 -0300

    upgrade

---
+++ ListaDeExercicios/script.js
@@ -0,0 +1,8 @@
+// Escreva um algoritmo que imprima os números pares de 1 a 20 usando um loop for.
+
+var inicio2=2;
+var final2=20;
+
+for (inicio2;inicio2<=final2;inicio2=inicio2+2) {
+    alert (inicio2)
+}
